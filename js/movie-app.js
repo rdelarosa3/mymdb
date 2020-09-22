ac390eb9ea80dd9bf472a006d5b9158dd009b5e1
Robert De la Rosa <rdelarosa39@gmail.com>
2020-09-22 15:56:21 -0500

    Added: feat delete all delete each

--- js/movie-app.js
+++ js/movie-app.js
@@ -1,8 +1,19 @@
 
-const top10Movies = [];
+const seedList = [
+    "avengers",
+    "the pest",
+    "nightmare before christmas",
+    "earnest scared stupid",
+    "the lord of the rings",
+    "star wars",
+    "IT",
+    "extraction",
+    "iron man",
+    "tenet"
+];
 
-const getMovie =(movie) => {
-    const url =`http://www.omdbapi.com/?t=${movie}&apikey=${OMDbKey}&`
+const getOmdb =(movie) => {
+    const url =`http://www.omdbapi.com/?t=${movie}&apikey=${OMDbkey}&`
     return fetch(url)
         .then(response => response.json())
         .then(movieData => {
@@ -11,32 +22,33 @@ const getMovie =(movie) => {
         .catch(error => console.log(error));
 }
 
-getMovie("avengers").then((data)=>{
-    console.log(data)
-    let newMovie = {
-        title: data.Title,
-        year:  data.Year,
-        rated: data.Rated,
-        released: data.Released,
-        runtime: data.Runtime,
-        genre: data.Genre,
-        plot: data.Plot,
-        language: data.Language,
-        poster: data.Plot,
-        rating: data.Ratings,
-        director: data.Director,
-        actors: data.Actors,
-    }
-    addMovie(newMovie);
-});
-
-
-newMovie = () =>{
-
+const seedData = ()=>{
+    for(let movie of seedList){
+        getOmdb(movie).then((data)=>{
+            console.log("ombd",data)
+            let newMovie = {
+                title: data.Title,
+                year:  data.Year,
+                rated: data.Rated,
+                released: data.Released,
+                runtime: data.Runtime,
+                genre: data.Genre,
+                plot: data.Plot,
+                language: data.Language,
+                poster: data.Plot,
+                rating: data.Ratings,
+                director: data.Director,
+                actors: data.Actors,
+            }
+            addMovie(newMovie);
+        });
+    } 
 }
+
+
+
 $("#createMovie").submit(function (event){
     event.preventDefault();
-
     let newMovie = {
         title: $("#title").val(),
         year:  $("#year").val(),
@@ -79,27 +91,25 @@ const addMovie = (movie)=>{
 }
 
 const createMovieCard = (movie) => {
-
-
     $('#database-list').append(`
-     <div id="movie.id" class="card" style="width: 18rem;">
-      <button class="updatebtn">Update</button>
-      <button class="deletebtn">Delete</button>
-      <div class="card-header">${movie.title}</div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Year: ${movie.year}</li>
-        <li class="list-group-item">Rated: ${movie.rated}</li>
-        <li class="list-group-item">Released: ${movie.released}</li>
-        <li class="list-group-item">Runtime: ${movie.runtime}</li>
-        <li class="list-group-item">Genre: ${movie.genre}</li>
-        <li class="list-group-item">Director: ${movie.director}</li>
-        <li class="list-group-item">Actors: ${movie.actors}</li>
-        <li class="list-group-item">Plot: ${movie.plot}</li>
-        <li class="list-group-item">Language: ${movie.language}</li>
-        <li class="list-group-item">Poster: ${movie.poster}</li>
-        <li class="list-group-item">Ratings: ${movie.ratings}</li>
-      </ul>
-    </div>
+        <div id="${movie.id}" class="card" style="width: 18rem;">
+            <button class="updatebtn">Update</button>
+            <button class="deletebtn">Delete</button>
+            <div class="card-header">${movie.title}</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Year: ${movie.year}</li>
+                <li class="list-group-item">Rated: ${movie.rated}</li>
+                <li class="list-group-item">Released: ${movie.released}</li>
+                <li class="list-group-item">Runtime: ${movie.runtime}</li>
+                <li class="list-group-item">Genre: ${movie.genre}</li>
+                <li class="list-group-item">Director: ${movie.director}</li>
+                <li class="list-group-item">Actors: ${movie.actors}</li>
+                <li class="list-group-item">Plot: ${movie.plot}</li>
+                <li class="list-group-item">Language: ${movie.language}</li>
+                <li class="list-group-item">Poster: ${movie.poster}</li>
+                <li class="list-group-item">Ratings: ${movie.ratings}</li>
+              </ul>
+        </div>
     `);
 }
 
@@ -125,4 +135,39 @@ const getDatabase =() => {
 getDatabase();
 
 
-// const {Title, Year, Rated, Released, Runtime, Genre, Director, Actors, Plot, Language, Poster, imdbRating, imdbID} = movieData;
+
+deleteMovie = (id)=>{
+    let url = `https://copper-cypress-bakery.glitch.me/movies/${id}`;
+    fetch(url,{
+        method: "DELETE"
+    })
+}
+
+deleteAll = () =>{
+    fetch('https://copper-cypress-bakery.glitch.me/movies')
+        .then((response)=> {
+            if (response.ok) {
+                return response.json();
+            }
+            return Promise.reject(response);
+        })
+        .then( (data)=> {
+            console.log("deleting data",data);
+            for (let movie of data){
+                deleteMovie(movie.id);
+            }
+        })
+        .catch( (error) =>{
+            console.warn("error", error);
+        });
+}
+
+
+$(document).on('click','.deletebtn',function(){
+    let id = $(this).parent()[0].id;
+    deleteMovie(id);
+});
+
+
+// finished delete movie funtions
+// working on ASYNC for refresh div
